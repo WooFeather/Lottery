8e8fc5fb1f8c7241e749e3257cfc1174c0a80523
Woohyun Jo <bluewing1004@gmail.com>
2025-02-24 15:05:37 +0900

    Initial Commit

---
+++ Lottery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lottery
+//
+//  Created by 조우현 on 2/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
